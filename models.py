4859887002c2b18f31207bb0f9ce2f4002a38bff
khoa nguyen dang <ndkhoa0704@gmail.com>
2023-04-24 09:26:35 +0700

    update

--- models.py
+++ models.py
@@ -4,15 +4,15 @@ from logger_config import logger
 
 
 class CustomTransSmth(nn.Module):
-    def __init__(self, gamma, dim, *args, **kwargs):
+    def __init__(self, dim, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dim = dim
         self.wr = nn.Parameter(torch.rand((self.dim, self.dim)), requires_grad=True)
-        # self.wh = nn.Parameter(torch.rand((self.dim, self.dim)), requires_grad=True)
-
+        self.wh = nn.Parameter(torch.rand((self.dim, self.dim)), requires_grad=True)
+        self.lamb = nn.Parameter(torch.rand((1,)), requires_grad=True)
 
     def forward(self, h, r):
-        t = h@self.wh + r
+        t = h@self.wh + r@self.wr + self.lamb
         return t
     
 
